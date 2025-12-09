b24363426aa287d1bf12703a565f5b19011933c1
alex <alex.nenakhov@yandex.ru>
2025-12-09 11:04:02 +0300

    сщлл

--- VisualStudioCode/SimpleCSharpConsoleApp/Program.cs
+++ VisualStudioCode/SimpleCSharpConsoleApp/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
 class Program
 {
     static void Main()
     {
-        var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        var evens = numbers.Where(n => n % 2 == 0);
-        Console.WriteLine("Четные числа: " + string.Join(", ", evens));
+        var numbers = new[] { 1, 2, 3, 4, 5 };
+        var squares = numbers.Select(n => n * n);
+        Console.WriteLine("Квадраты: " + string.Join(", ", squares));
     }
 }
